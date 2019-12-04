9695aa6953c0cbd97ccd9be70e16f252f68a3b6e
Florian Martet <florian.martet@ca.thalesgroup.com>
2019-12-04 17:37:40 -0500

    add fish/i3/omf/polybar configs

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+
+set -g theme_nerd_fonts yes
+set -gx PATH ~/.cargo/bin $PATH
+
+alias k kubectl
+alias kd 'k describe'
+alias kg 'k get'
+alias kl 'k logs'
---
+++ fish/functions/fish_prompt.fish
@@ -0,0 +1,1 @@
+/home/t0138316/.local/share/omf/themes/bobthefish/fish_prompt.fish
